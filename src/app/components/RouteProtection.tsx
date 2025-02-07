06a04725a3a0069ef6a10563ab2ade104abc15e0
j27amna <amnajehanzeb27@gmail.com>
2025-02-08 02:01:20 +0500

    initial commit

---
+++ src/app/components/RouteProtection.tsx
@@ -0,0 +1,17 @@
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function RouteProtection ({children} : {children : React.ReactNode}) {
+    const router = useRouter()
+    
+    useEffect(() => {
+        const isLoggedIn = localStorage.getItem('isLoggedIn')
+        if (!isLoggedIn) {
+            router.push('/login')
+        }
+    }, [router])
+
+    return <>
+    {children}
+    </>;
+}
